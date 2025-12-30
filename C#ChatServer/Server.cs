d54bfee05aa66f865facc33ac94bf621000db2be
YunJuho <younv2@naver.com>
2025-12-30 18:38:17 +0900

    [DEV] C# 콘솔 채팅 서버 작업중

---
+++ C#ChatServer/Server.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+class Server
+{
+    static void Main()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 9000);
+        listener.Start();
+
+        Socket listenSocket =  listener.Server;
+
+        List<Socket> clientSockets = new List<Socket>();
+        List<Socket> readList = new List<Socket>();
+
+        byte[] buf = new byte[1024];
+        while (true)
+        {
+            readList.Clear();
+            readList.Add(listenSocket);
+            readList.AddRange(clientSockets);
+
+            // select + fd_set 대응 == Socket.Select(...)
+            // 무한 대기(이벤트 오면 넘어감)
+            Socket.Select(readList, null, null, -1);
+
+            // 리슨 소켓에 이벤트가 발생했을 경우
+            if (readList.Contains(listenSocket))
+            {
+                Socket client = listenSocket.Accept(); // accept == AcceptSocket()
+                clientSockets.Add(client);
+            }
+
+            for (int i = clientSockets.Count - 1; i >= 0; i--)
+            {
+                Socket client = clientSockets[i];
+
+                //클라이언트 소켓에 이벤트가 발생했을 경우
+                if (readList.Contains(client))
+                {
+                    int recvLen;
+
+                    recvLen = client.Receive(buf);
+
+
+                    if (recvLen <= 0)
+                    {
+                        client.Close();
+                        clientSockets.RemoveAt(i);
+                        continue;
+                    }
+
+                    client.Send(buf, 0, recvLen, SocketFlags.None);
+
+                    client.Close();
+                    clientSockets.RemoveAt(i);
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ Client/Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json.Serialization;
+
+class Client
+{
+    static void Main()
+    {
+        using var client = new TcpClient("127.0.0.1", 9000);
+        using NetworkStream stream = client.GetStream();
+
+        ChatManager chatManager= new ChatManager();
+
+        string nickname = Console.ReadLine() ?? "null";
+
+        chatManager.Send(stream, chatManager.MakePacket("SetNickname",nickname));
+
+        //받기 처리
+
+        Console.WriteLine("echo: " + Encoding.UTF8.GetString(recvBuf, 0, n));
+    }
+}
+public class ChatManager
+{
+    public enum ChatProtocol : byte
+    {
+        SetNickName,
+        Message,
+        Notice
+    }
+    private ChatMessage m_Chat;
+
+    public ChatManager()
+    {
+        m_Chat = new ChatMessage();
+    }
+
+    public byte[] MakePacket(string protocol,params object[] objects)
+    {
+        byte[] packet = new byte[1];
+        if (protocol == "SetNickname")
+        {
+            if (objects == null || objects.Length < 1)
+                return packet;
+            byte[] nicknameBytes = Encoding.UTF8.GetBytes(objects[0].ToString());
+            if (nicknameBytes.Length > 255)
+            {
+                return packet;
+            }
+            packet = new byte[2 + nicknameBytes.Length];
+            packet[0] = (byte)ChatProtocol.SetNickName;
+            packet[1] = (byte)nicknameBytes.Length;
+            Array.Copy(nicknameBytes, 0, packet, 2, nicknameBytes.Length);
+        }
+
+        return packet;
+    }
+    public void SetNicknameOnResponse(string _nickname)
+    {
+        // 닉네임 설정 응답 처리
+        m_Chat.Nickname = _nickname;
+    }
+    public void Send(NetworkStream _stream, byte[] _bytes)
+    {
+        _stream.Write(_bytes, 0, _bytes.Length);
+    }
+}
+public struct ChatMessage
+{
+    public string Nickname;
+    public string Message;
+
+}
