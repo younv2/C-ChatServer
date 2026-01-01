cbebe3e7e45c5098db31d324f41948b7971f0d63
younv2 <younv2@naver.com>
2026-01-02 02:06:11 +0900

    [DEV] 채팅 서버 구조 변경

---
+++ C#ChatServer/ChatServer.cs
@@ -0,0 +1,130 @@
+using Network;
+using System.Net;
+using System.Net.Sockets;
+
+internal class ChatServer
+{
+    private readonly int m_Port;
+    //TCPListener안에 Socket이 이미 포함됨(리슨소켓)
+    private TcpListener m_Listener;
+    private List<Socket> m_ClientSockets = new();
+    private bool m_IsRunning;
+
+    public ChatServer(int _port)
+    {
+        m_Port = _port;
+        m_Listener = new TcpListener(IPAddress.Any, m_Port);
+    }
+
+    public void Start()
+    {
+        m_Listener.Start();
+
+        m_IsRunning = true;
+        Console.WriteLine($"채팅 서버가 {m_Port} 포트에서 시작되었습니다.");
+
+        Run();
+    }
+    private void Run()
+    {
+        List<Socket> readList = new List<Socket>();
+
+        while (m_IsRunning)
+        {
+            readList.Clear();
+            
+            readList.Add(m_Listener.Server);
+            readList.AddRange(m_ClientSockets);
+            Socket.Select(readList, null, null, 1000);
+
+            foreach (var sock in readList)
+            {
+                if (sock == m_Listener.Server)
+                {
+                    AcceptNewClient();
+                }
+                else
+                {
+                    HandleClientData(sock);
+                }
+            }
+        }
+    }
+    private void AcceptNewClient()
+    {
+        Socket client = m_Listener.AcceptSocket();
+        m_ClientSockets.Add(client);
+        // client.RemoteEndPoint == 클라이언트의 IP주소와 포트번호
+        Console.WriteLine($"[접속] {client.RemoteEndPoint}");
+    }
+    private void HandleClientData(Socket client)
+    {
+        while (client.Available >= 3)
+        {
+            var result = ProcessPacket(client);
+
+            if (result == PacketResult.Pending)
+            {
+                break;
+            }
+            else if (result == PacketResult.Error)
+            {
+                RemoveClient(client);
+                break;
+            }
+            
+        }
+    }
+    private PacketResult ProcessPacket(Socket client)
+    {
+        try
+        {
+            if (!NetworkSystem.TryPeekHeader(client, out var header)) 
+                return PacketResult.Pending;
+            if (client.Available < header.Size) 
+                return PacketResult.Pending;
+
+            switch (header.Protocol)
+            {
+                case ChatProtocol.SetNickname:
+                    var nicknamePacket = NetworkSystem.ReadPacket<ReqChatDataPacket>(client, header.Size);
+                    Console.WriteLine($"[로그] [{DateTime.Now}]{nicknamePacket.ChatData.Nickname}님이 닉네임을 설정함.");
+                    break;
+
+                case ChatProtocol.Message:
+                    var msgPacket = NetworkSystem.ReadPacket<ReqChatDataPacket>(client, header.Size);
+                    Console.WriteLine($"[채팅] [{DateTime.Now}]{msgPacket.ChatData.Nickname}: {msgPacket.ChatData.Msg}");
+                    Broadcast(ByteConverter.StructureToBytes(msgPacket));
+                    break;
+            }
+            return PacketResult.Success;
+        }
+        catch
+        {
+            Console.WriteLine($"[오류] [{DateTime.Now}]클라이언트가 비정상적으로 종료됨.");
+            return PacketResult.Error;
+        }
+    }
+
+    private void Broadcast(byte[] data)
+    {
+        for (int i = m_ClientSockets.Count - 1; i >= 0; i--)
+        {
+            try
+            {
+                NetworkSystem.Send(m_ClientSockets[i], data);
+            }
+            catch
+            {
+                RemoveClient(m_ClientSockets[i]);
+            }
+        }
+    }
+
+    private void RemoveClient(Socket socket)
+    {
+        Console.WriteLine($"[퇴장] {socket.RemoteEndPoint}");
+        m_ClientSockets.Remove(socket);
+        socket.Close();
+    }
+}
--- C#ChatServer/Server.cs
+++ C#ChatServer/Server.cs
@@ -1,72 +1,8 @@
-using System.Net;
-using System.Net.Sockets;
-using Network;
 class Server
 {
     static void Main()
     {
-        TcpListener listener = new TcpListener(IPAddress.Any, 9000);
-        listener.Start();
-
-        Socket listenSocket = listener.Server;
-
-        List<Socket> clientSockets = new List<Socket>();
-        List<Socket> readList = new List<Socket>();
-
-        byte[] recvBuf = new byte[4096];
-        while (true)
-        {
-            readList.Clear();
-            readList.Add(listenSocket);
-            readList.AddRange(clientSockets);
-
-            // select + fd_set 대응 == Socket.Select(...)
-            // 무한 대기(이벤트 오면 넘어감)
-            Socket.Select(readList, null, null, -1);
-
-            // 리슨 소켓에 이벤트가 발생했을 경우
-            if (readList.Contains(listenSocket))
-            {
-                Socket client = listenSocket.Accept(); // accept == AcceptSocket()
-                clientSockets.Add(client);
-            }
-
-            for (int i = clientSockets.Count - 1; i >= 0; i--)
-            {
-                Socket client = clientSockets[i];
-
-                //클라이언트 소켓에 이벤트가 발생했을 경우
-                if (readList.Contains(client))
-                {
-                    while(true)
-                    {
-                        if (NetworkSystem.TryPeekHeader(client, out var header))
-                        {
-                            if (header.Protocol == ChatProtocol.SetNickname)
-                            {
-                                var data = NetworkSystem.ReadPacket<ReqChatDataPacket>(client);
-
-                                Console.WriteLine($"{data.ChatData.Nickname}닉네임을 설정했습니다.");
-                                break;
-                            }
-                            else if (header.Protocol == ChatProtocol.Message)
-                            {
-                                var data = NetworkSystem.ReadPacket<ReqChatDataPacket>(client);
-
-                                Console.WriteLine($"{data.ChatData.Nickname} : {data.ChatData.Msg}");
-
-                                var sendData = ByteConverter.StructureToBytes(data);
-                                // 클라이언트에게 메시지 뿌리기
-                                foreach (var targetClient in clientSockets)
-                                {
-                                    NetworkSystem.Send(targetClient, sendData);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        ChatServer chatServer = new ChatServer(9000);
+        chatServer.Start();
     }
 }
